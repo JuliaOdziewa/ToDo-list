86681b7e1b04a5e80261f9eb33478b3d0c04e1d0
Julia Odziewa <julia.odziewa@gmail.com>
2021-07-06 19:12:11 +0200

    changed toggleTaskDone function

--- js/script.js
+++ js/script.js
@@ -18,7 +18,14 @@
     };
 
     const toggleTaskDone = (taskIndex) => {
-        tasks[taskIndex].done = !tasks[taskIndex].done;
+        tasks = [
+            ...task.slice(0, taskIndex),
+            {
+                ...tasks[taskIndex],
+                done: !tasks[taskIndex].done,
+            },
+            ...tasks.slice(taskIndex + 1),
+        ];
         render();
     };
 
